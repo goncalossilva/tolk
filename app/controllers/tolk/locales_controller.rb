09ae289c310b899f95030977cde12b599fc213c0
Gonçalo Silva <goncalossilva@gmail.com>
2011-10-13 03:19:58 +0100

    add option to select the amount of translations per page

--- app/controllers/tolk/locales_controller.rb
+++ app/controllers/tolk/locales_controller.rb
@@ -3,6 +3,7 @@ module Tolk
     before_filter :find_locale, :only => [:show, :all, :update, :updated]
     before_filter :find_from_locale, :only => [:show, :all]
     before_filter :ensure_no_primary_locale, :only => [:all, :update, :show, :updated]
+    before_filter :update_per_page, :only => [:all, :show, :updated]
 
     def index
       @locales = Tolk::Locale.secondary_locales
@@ -49,10 +50,17 @@ module Tolk
     def find_locale
       @locale = Tolk::Locale.find_by_name!(params[:id])
     end
-    
+
     def find_from_locale
       @from_locale = Tolk::Locale.find_by_name(params[:from_locale]) || Tolk::Locale.find_by_name(cookies['tolk_from_locale']) || Tolk::Locale.primary_locale
       cookies['tolk_from_locale'] = params[:from_locale] if params[:from_locale]
+    end
+
+    def update_per_page
+      if params[:per_page]
+        params[:per_page] = 9999999 if params[:per_page] == "all"
+        Tolk::Phrase.per_page = params[:per_page].to_i if params[:per_page].to_i > 0
+      end
     end
   end
 end
